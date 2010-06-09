e505b208f75ccbef7b3c9972e84117eb6c1f1066
Matthew Curry <mjcurry@gmail.com>
2010-06-09 02:23:15 -0400

    Better (new-lisp-package), makes a file for tests now

--- currys-customizations/currys-lisp.el
+++ currys-customizations/currys-lisp.el
@@ -24,25 +24,33 @@
     (let ((oldbuf (current-buffer))
           (asd  (concat name ".asd"))
           (file (concat name ".lisp"))
+          (tests (concat name "-tests.lisp"))
           (pack "package.lisp"))
       (when (file-exists-p asd)  (error "File %s already exists!" asd))
       (when (file-exists-p file) (error "File %s already exists!" file))
+      (when (file-exists-p tests) (error "File %s already exists!" tests))
       (when (file-exists-p pack) (error "File %s already exists!" pack))
       (find-file asd)
       (goto-char 0)
       (insert ";;;; -*-Lisp-*- " asd "\n")
       (insert "(in-package :cl-user)\n\n")
-      (insert "(defpackage :" name "-asd (:use :cl))\n")
-      (insert "(in-package :" name "-asd)\n\n")
+      (insert "(defpackage :" name "-system (:use :cl))\n")
+      (insert "(in-package :" name "-system)\n\n")
       (insert "(asdf:defsystem :" name "\n"
-              "  :version \"0.0.1\"\n"
+              "  :serial t\n"
+	      "  ;; external dependencies go here\n"
+              "  :depends-on (:alexandria)\n"
+              "  :components \n"
+	      "   ((:file \"" (file-name-sans-extension pack) "\")\n"
+	      "    (:file \"" (file-name-sans-extension file) "\")))\n\n")
+      (insert "(asdf:defsystem :" name "-tests\n"
               "  :serial t\n"
 	      "  ;; external dependencies go here\n"
               "  :depends-on (:alexandria\n"
-	      "               :cl-fad)\n"
+	      "               :sb-rt\n"
+	      "               :" name ")\n"
               "  :components \n"
-	      "   ((:file \"" (file-name-sans-extension pack) "\")\n"
-	      "    (:file \"" (file-name-sans-extension file) "\")))\n")
+	      "   ((:file \"" (file-name-sans-extension tests) "\")))\n")
       (lisp-mode)
       (font-lock-mode)
       (save-buffer (current-buffer))
@@ -51,9 +59,8 @@
       (insert ";;;; " pack "\n\n")
       (insert "(in-package :common-lisp-user)\n\n")
       (insert "(defpackage :" name "\n")
-      (insert "  (:nicknames :" name ")\n")
       (insert "  ;; insert other packages below\n")
-      (insert "  (:use :cl)\n")
+      (insert "  (:use :cl :alexandria)\n")
       (insert "  ;; export symbols here\n")
       (insert "  (:export    ))\n\n")
       (save-buffer (current-buffer))
@@ -61,6 +68,14 @@
       (goto-char 0)
       (insert ";;;; " file "\n\n")
       (insert "(in-package :" name ")\n\n")
+      (save-buffer (current-buffer))
+      (find-file tests)
+      (goto-char 0)
+      (insert ";;;; " tests "\n\n")
+      (insert "(require :sb-rt)\n")
+      (insert "(defpackage :" name "-tests\n")
+      (insert "  (:use :cl :alexandria :sb-rt :" name "))\n\n")
+      (insert "(in-package :" name "-tests)\n\n")
       (save-buffer (current-buffer)))))
 
 (provide 'currys-lisp)
